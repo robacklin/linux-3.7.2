47011c3d0ead95a8a5a8a6d3eee802c7176637a5
robacklin <rob.acklin@gmail.com>
2013-05-22 23:09:44 -0400

    more script fixes

--- quick-make.sh
+++ quick-make.sh
@@ -5,8 +5,12 @@ if [ -z "${CFG_TARGET}" ] ; then
 	echo "CFG_TARGET=${CFG_TARGET}"
 fi
 if [ -z "${CFG_SOURCE}" ] ; then
-	CFG_SOURCE=`pwd`/build
+	CFG_SOURCE=`pwd`/source
 	echo "CFG_SOURCE=${CFG_SOURCE}"
+fi
+if [ -z "${CFG_BUILD}" ] ; then
+	CFG_BUILD=`pwd`/build
+	echo "CFG_BUILD=${CFG_BUILD}"
 fi
 if [ -z "${CFG_LINUX_CONFIG}" ] ; then
 	CFG_LINUX_CONFIG=i386_defconfig
@@ -19,12 +23,13 @@ fi
 
 mkdir -p $CFG_TARGET
 mkdir -p $CFG_SOURCE
+mkdir -p $CFG_BUILD
 
 cd $CFG_SOURCE
-make O=$CFG_TARGET mrproper || exit 1
-make O=$CFG_TARGET $CFG_LINUX_CONFIG || exit 1
-make O=$CFG_TARGET dep || exit 1
-make O=$CFG_TARGET clean || exit 1
-make O=$CFG_TARGET $CFG_LINUX_IMAGE modules || exit 1
-make O=$CFG_TARGET modules_install || exit 1
+make O=$CFG_BUILD mrproper || exit 1
+make O=$CFG_BUILD $CFG_LINUX_CONFIG || exit 1
+make O=$CFG_BUILD dep || exit 1
+make O=$CFG_BUILD clean || exit 1
+make O=$CFG_BUILD $CFG_LINUX_IMAGE modules || exit 1
+make O=$CFG_BUILD INSTALL_MOD_PATH=$CFG_TARGET modules_install || exit 1
 exit 0
