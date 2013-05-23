910b2e2ab2ae004b198124c0e29952e7442a8869
robacklin <rob.acklin@gmail.com>
2013-05-22 22:35:43 -0400

    more script fixes

--- quick-make.sh
+++ quick-make.sh
@@ -2,27 +2,27 @@
 
 if [ -z "${CFG_TARGET}" ] ; then
 	CFG_TARGET=`pwd`/target
-	echo "${CFG_TARGET}"
+	echo "CFG_TARGET=${CFG_TARGET}"
 fi
 if [ -z "${CFG_SOURCE}" ] ; then
 	CFG_SOURCE=`pwd`/build
-	echo "${CFG_SOURCE}"
+	echo "CFG_SOURCE=${CFG_SOURCE}"
 fi
 if [ -z "${CFG_LINUX_CONFIG}" ] ; then
 	CFG_LINUX_CONFIG=i386_defconfig
-	echo "${CFG_LINUX_CONFIG}"
+	echo "CFG_LINUX_CONFIG=${CFG_LINUX_CONFIG}"
 fi
 if [ -z "${CFG_LINUX_IMAGE}" ] ; then
 	CFG_LINUX_IMAGE=bzImage
-	echo "${CFG_LINUX_IMAGE}"
+	echo "CFG_LINUX_IMAGE=${CFG_LINUX_IMAGE}"
 fi
 
 mkdir -p $CFG_TARGET
 mkdir -p $CFG_SOURCE
 
 cd $CFG_SOURCE
-make mrproper || exit 1
-make $CFG_LINUX_CONFIG || exit 1
+make O=$CFG_TARGET mrproper || exit 1
+make O=$CFG_TARGET $CFG_LINUX_CONFIG || exit 1
 make O=$CFG_TARGET dep || exit 1
 make O=$CFG_TARGET clean || exit 1
 make O=$CFG_TARGET $CFG_LINUX_IMAGE modules || exit 1
